146dc7dc4883f7e6e6b37ece5c66384eeaca5d92
phanuwat-pao <phanuwat.lua@gmail.com>
2024-09-25 11:40:47 +0700

    Refactor project configuration files and update database connection to use @vercel/postgres

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,22 @@
+// import { type Config } from "drizzle-kit";
+
+// import { env } from "~/env";
+
+// export default {
+//   schema: "./src/server/db/schema.ts",
+//   dialect: "postgresql",
+//   dbCredentials: {
+//     url: env.DATABASE_URL,
+//   },
+//   tablesFilter: ["open-his_*"],
+// } satisfies Config;
 import { type Config } from "drizzle-kit";
 
 export default {
   schema: "./src/server/db/schema.ts",
   dialect: "postgresql",
-  // dbCredentials: {
-  //   url: env.DATABASE_URL,
-  // },
+  dbCredentials: {
+    url: process.env.POSTGRES_URL!,
+  },
   tablesFilter: ["open-his_*"],
 } satisfies Config;
